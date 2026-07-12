0217127f4fa71585c34a75f2586df8da152496e1
Tushar Gupta <tusharg.mp1@gamil.com>
2026-07-12 21:54:25 +0530

    fix the blade error

--- day-3-React3Fiber-project/src/components/FanGallery.jsx
+++ day-3-React3Fiber-project/src/components/FanGallery.jsx
@@ -5,60 +5,67 @@ import ImagePlane from "./ImagePlane";
 import { useFrame } from "@react-three/fiber";
 
 const FanGallery = () => {
-  const { planeHeight, planeWidth, numPlanes, spreadAngle, positionY,rotationY } =
-    useControls("book fan", {
-      planeWidth: {
-        value: 2.5,
-        min: 0.4,
-        max: 6,
-        step: 0.05,
-        label: "plane width",
-      },
-      planeHeight: {
-        value: 2.5,
-        min: 0.4,
-        max: 8,
-        step: 0.05,
-        label: "plane height",
-      },
-      numPlanes: {
-        value: 7,
-        min: 2,
-        max: 40,
-        step: 1,
-        label: "No of blades",
-      },
-      positionY: {
-        value: -1.5,
-        min: -6,
-        max: 6,
-        step: 0.05,
-        label: "Y Position",
-      },
-      spreadAngle: {
-        value: 360,
-        min: 20,
-        max: 360,
-        step: 1,
-        label: "Spread Angle",
-      },
-      rotationY:{
-        value:0.5,
-        min:-0.8,
-        max:0.8,
-        step:0.01,
-        label:'rotation Speed'
-      }
-    });
+  const {
+    planeHeight,
+    planeWidth,
+    numPlanes,
+    spreadAngle,
+    positionY,
+    rotationY,
+  } = useControls("book fan", {
+    planeWidth: {
+      value: 2.5,
+      min: 0.4,
+      max: 6,
+      step: 0.05,
+      label: "plane width",
+    },
+    planeHeight: {
+      value: 2.5,
+      min: 0.4,
+      max: 8,
+      step: 0.05,
+      label: "plane height",
+    },
+    numPlanes: {
+      value: 7,
+      min: 2,
+      max: 40,
+      step: 1,
+      label: "No of blades",
+    },
+    positionY: {
+      value: -1.5,
+      min: -6,
+      max: 6,
+      step: 0.05,
+      label: "Y Position",
+    },
+    spreadAngle: {
+      value: 360,
+      min: 20,
+      max: 360,
+      step: 1,
+      label: "Spread Angle",
+    },
+    rotationY: {
+      value: 0.5,
+      min: -0.8,
+      max: 0.8,
+      step: 0.01,
+      label: "rotation Speed",
+    },
+  });
 
   const planes = useMemo(() => {
     const count = numPlanes;
     const totalArcRad = (spreadAngle * Math.PI) / 180;
     const step = totalArcRad / (count - 1);
-    const startingAngle = - totalArcRad / 2;
 
-    return Array.from({ length: 20 }, (_, i) => {
-      const angle = startingAngle + i * step;
+    const start = -totalArcRad / 2;
+
+    return Array.from({ length: numPlanes }, (_, i) => {
+      const angle = start + i * step;
       return {
         key: i,
         url: images[i % images.length],
@@ -66,16 +73,16 @@ const FanGallery = () => {
         rotation: [0, angle, 0],
       };
     });
-  }, [numPlanes, spreadAngle,rotationY]);
+  }, [numPlanes, spreadAngle, rotationY]);
 
   const groupRef = useRef(null);
 
-  useFrame((state,delta)=>{
-    groupRef.current.rotation.y += delta * rotationY
-  })
+  useFrame((state, delta) => {
+    groupRef.current.rotation.y += delta * rotationY;
+  });
 
   return (
-    <group ref={groupRef} position={[0,positionY,0]}>
+    <group ref={groupRef} position={[0, positionY, 0]}>
       {planes.map((plane) => (
         <ImagePlane
           key={plane.key}
